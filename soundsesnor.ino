0d6122bb14b7491453e7244f5ce70a500f938fb8
monicaagarwal <monica.agarwal8937@gmail.com>
2016-10-10 10:28:48 +0530

    Add files via upload

---
+++ soundsesnor.ino
@@ -0,0 +1,26 @@
+int Led=13;
+int btn=4;
+int val=0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(Led,OUTPUT);
+  pinMode(btn,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val=digitalRead(btn);
+  if(val==HIGH)
+  {
+    
+    digitalWrite(Led,HIGH);
+  }
+  else
+  {
+    digitalWrite(Led,LOW);
+  }
+
+}
